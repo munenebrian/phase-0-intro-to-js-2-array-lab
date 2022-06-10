c12ba517eedbc14b9ba66755b6bd09ef29f8b5a5
munenebrian <njerubrianmunene@gmail.com>
2022-06-10 15:17:18 +0300

    commit tree 8be01fafe13b2f163dd55b1b155b4009c48b1f04

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(Garfield) {
+    return [...cats, Garfield];
+}
+
+function prependCat(Milo) {
+    return [Milo, ...cats];
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
